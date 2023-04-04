3d32582b28e53a8367c4c9412f2eea9e5aaf4923
Amos <amos2580@hotmail.co.uk>
2023-04-04 22:27:41 +0100

    Final

--- IW4MAdminDatabaseMigration/Migration.cs
+++ IW4MAdminDatabaseMigration/Migration.cs
@@ -1,4 +1,4 @@
-﻿using Data.MigrationContext;
+﻿using Data.Context;
 using Data.Models;
 using Data.Models.Client;
 using Data.Models.Client.Stats;
@@ -7,52 +7,89 @@ using Data.Models.Misc;
 using Data.Models.Server;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata;
+using MySqlConnector;
+using Npgsql;
 
 namespace IW4MAdminDatabaseMigration;
 
-internal class Migration
+internal static class Migration
 {
-    private const int BatchSize = 1_000;
+    private const int BatchSize = 10_000;
 
-    public static async Task MigrateDataAsync(SqliteDatabaseContext sourceContext, PostgresqlDatabaseContext targetContext)
+    public static async Task MigrateDataAsync(DatabaseContext sourceContext, Func<DatabaseContext> targetContextFunc)
     {
-        Console.WriteLine("\n\nPlease wait until the program exists before closing... This will take time!\n\n");
-        Console.WriteLine("Applying IW4MAdmin database migrations...");
-        await targetContext.Database.MigrateAsync();
+        sourceContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
 
-        var tableDependencyOrder = GetTableDependencyOrder(targetContext);
+        Console.WriteLine("Please wait until the program exits before closing... This will take time!");
+        Console.WriteLine();
+
+        await ApplyMigrations(sourceContext, targetContextFunc);
+
+        var tableDependencyOrder = GetTableDependencyOrder(targetContextFunc());
+
         Console.WriteLine($"Migrating {tableDependencyOrder.Count} tables... Please wait...");
-
-        var index = 0;
-        foreach (var dbSetProperty in tableDependencyOrder.Select(tableType => sourceContext.GetType().GetProperties()
-                         .FirstOrDefault(p => p.PropertyType.IsGenericType &&
-                                              p.PropertyType.GetGenericTypeDefinition() == typeof(DbSet<>) &&
-                                              p.PropertyType.GenericTypeArguments[0] == tableType))
-                     .Where(dbSetProperty => dbSetProperty != null))
-        {
-            if (dbSetProperty?.GetValue(sourceContext) is not IQueryable<object> data) continue;
-            index++;
-            var count = 0;
-            var totalCount = await data.CountAsync();
-
-            while (true)
-            {
-                var batch = await data.AsNoTracking().Skip(count).Take(BatchSize).ToListAsync();
-                if (!batch.Any()) break;
-
-                await targetContext.AddRangeAsync(batch);
-                await targetContext.SaveChangesAsync();
-
-                count += batch.Count;
-                Console.WriteLine(
-                    $"[{DateTimeOffset.UtcNow:yyyy-MM-dd HH:mm:ss}] {dbSetProperty.Name} ({index}/{tableDependencyOrder.Count}): " +
-                    $"Completed ({count:N0}/{totalCount:N0})");
-            }
-        }
+        await MigrateTables(sourceContext, targetContextFunc, tableDependencyOrder);
 
         Console.WriteLine("All tables migrated.");
+        Console.WriteLine("Change IW4MAdminConfigurationSettings.json to reflect the new database.");
+        Console.WriteLine("Press any key to exit.");
         Console.ReadKey();
     }
+
+    private static async Task ApplyMigrations(DbContext sourceContext, Func<DatabaseContext> targetContextFunc)
+    {
+        Console.WriteLine("Applying migrations to SOURCE database...");
+        await sourceContext.Database.MigrateAsync();
+
+        Console.WriteLine("Applying migrations to TARGET database...");
+        await using var targetContext = targetContextFunc();
+        await targetContext.Database.MigrateAsync();
+    }
+
+    private static async Task MigrateTables(IAsyncDisposable sourceContext, Func<DatabaseContext> targetContextFunc,
+        IReadOnlyList<Type> tableDependencyOrder)
+    {
+        for (var i = 0; i < tableDependencyOrder.Count; i++)
+        {
+            var tableType = tableDependencyOrder[i];
+            var data = sourceContext.GetType().GetMethod("Set", Array.Empty<Type>())?
+                .MakeGenericMethod(tableType).Invoke(sourceContext, null) as IQueryable<object>;
+            if (data is null) continue;
+            var totalCount = await data.CountAsync();
+
+            int count, processedCount = 0;
+            for (count = 0; count < totalCount; count += BatchSize)
+            {
+                var batch = await data.AsNoTracking()
+                    .Skip(count)
+                    .Take(BatchSize)
+                    .ToListAsync();
+
+                processedCount += batch.Count;
+                await using var targetContext = targetContextFunc();
+                targetContext.ChangeTracker.AutoDetectChangesEnabled = false;
+                targetContext.AddRange(batch);
+                try
+                {
+                    await targetContext.SaveChangesAsync();
+                }
+                catch (DbUpdateException e) when (e.InnerException is
+                                                      PostgresException {SqlState: "23505"}
+                                                      or MySqlException {ErrorCode: MySqlErrorCode.DuplicateKeyEntry})
+                {
+                    Console.WriteLine("ERROR: Data already exists in target database. Please target another database...");
+                    Console.WriteLine("Press any key to exit.");
+                    Console.ReadKey();
+                    Environment.Exit(1);
+                }
+
+                Console.WriteLine(
+                    $"[{DateTimeOffset.UtcNow:yyyy-MM-dd HH:mm:ss}] {tableType.Name} " +
+                    $"({i + 1}/{tableDependencyOrder.Count}): Completed ({processedCount:N0}/{totalCount:N0})");
+            }
+        }
+    }
+
 
     private static List<Type> GetTableDependencyOrder(DbContext context)
     {
@@ -82,9 +119,10 @@ internal class Migration
             typeof(EFServerSnapshot),
             typeof(EFClientConnectionHistory),
         };
+
         var orderedTableTypes = new List<Type>();
-        var tables = context.Model.GetEntityTypes();
-        var entityTypes = tables as IEntityType[] ?? tables.ToArray();
+        var entityTypes = context.Model.GetEntityTypes().ToArray();
+
         foreach (var tableType in allTableTypes)
         {
             AddTableWithDependencies(orderedTableTypes, entityTypes, tableType, new HashSet<Type>());
@@ -93,14 +131,13 @@ internal class Migration
         return orderedTableTypes;
     }
 
-    private static void AddTableWithDependencies(ICollection<Type> orderedTableTypes, IEnumerable<IEntityType> tables, Type tableType,
+    private static void AddTableWithDependencies(ICollection<Type> orderedTableTypes, IEntityType[] entityTypes, Type tableType,
         ISet<Type> visitedTableTypes)
     {
         if (orderedTableTypes.Contains(tableType) || visitedTableTypes.Contains(tableType)) return;
 
         visitedTableTypes.Add(tableType);
 
-        var entityTypes = tables as IEntityType[] ?? tables.ToArray();
         var table = entityTypes.FirstOrDefault(t => t.ClrType == tableType);
         if (table == null) return;
 
--- IW4MAdminDatabaseMigration/Program.cs
+++ IW4MAdminDatabaseMigration/Program.cs
@@ -1,46 +1,126 @@
 ﻿using System.Reflection;
+using Data.Context;
 using Data.MigrationContext;
 using Microsoft.EntityFrameworkCore;
 
 namespace IW4MAdminDatabaseMigration;
 
-public class IW4MAdminDatabaseMigration
+public static class IW4MAdminDatabaseMigration
 {
     public static async Task Main()
     {
+        PrintHeader();
+
         var executingDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
         var directory = Path.Join(executingDirectory, "DatabaseSource");
-        if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
-        if (!File.Exists(Path.Join(directory, "Database.db")))
-        {
-            Console.WriteLine("Database.db doesn't exist in DatabaseSource directory.");
-            Console.ReadKey();
-            Environment.Exit(1);
-        }
 
-        if (!File.Exists(Path.Join(executingDirectory, "_ConnectionString.txt")))
-        {
-            const string defaultConnectionString = "Host=HOSTNAME;Port=PORT;Username=USERNAME;Password=PASSWORD;Database=DATABASE";
-            await File.WriteAllTextAsync(Path.Join(executingDirectory, "_ConnectionString.txt"), defaultConnectionString);
-            Console.WriteLine("Connection string file doesn't exist. _ConnectionString.txt has been created.");
-            Console.ReadKey();
-            Environment.Exit(1);
-        }
+        ValidateDatabaseDirectory(directory);
+        BackupDatabaseIfExists(directory);
 
-        var connectionString = await File.ReadAllTextAsync(Path.Join(executingDirectory, "_ConnectionString.txt"));
+        var connectionString = await GetConnectionString(executingDirectory!);
 
         var sourceOptions = new DbContextOptionsBuilder<SqliteDatabaseContext>()
             .UseSqlite("Data Source=DatabaseSource\\Database.db")
             .Options;
 
-        var targetOptions = new DbContextOptionsBuilder<PostgresqlDatabaseContext>()
+        var sourceContext = new SqliteDatabaseContext(sourceOptions);
+        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+
+        Console.WriteLine();
+        Console.WriteLine("Migration to:");
+        Console.WriteLine("1) PostgreSQL");
+        Console.WriteLine("2) MySQL/MariaDB");
+        Console.Write("Select: [1 or 2] ");
+        var input = Console.ReadLine();
+        Console.WriteLine();
+
+        switch (input)
+        {
+            case "1":
+                await MigrateToPostgreSql(connectionString, sourceContext);
+                break;
+            case "2":
+                await MigrateToMySql(connectionString, sourceContext);
+                break;
+            default:
+                Console.WriteLine("Invalid input. Exiting...");
+                Console.ReadKey();
+                Environment.Exit(1);
+                break;
+        }
+    }
+
+    private static void PrintHeader()
+    {
+        Console.WriteLine();
+        Console.WriteLine("=====================================================");
+        Console.WriteLine(" IW4MAdmin Database Migration");
+        Console.WriteLine(" by Ayymoss#8334 ");
+        Console.WriteLine($" Version {Assembly.GetExecutingAssembly().GetName().Version}");
+        Console.WriteLine("=====================================================");
+        Console.WriteLine();
+    }
+
+    private static void ValidateDatabaseDirectory(string directory)
+    {
+        if (!Directory.Exists(directory)) Directory.CreateDirectory(directory);
+        if (File.Exists(Path.Join(directory, "Database.db"))) return;
+        Console.WriteLine("Database.db doesn't exist in DatabaseSource directory.");
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+        Environment.Exit(1);
+    }
+
+    private static void BackupDatabaseIfExists(string directory)
+    {
+        Console.WriteLine("Backing up Database.db...");
+        try
+        {
+            File.Copy(Path.Join(directory, "Database.db"), Path.Join(directory, $"Database-{DateTimeOffset.UtcNow.ToFileTime()}.db"), true);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: {0}", e);
+        }
+    }
+
+    private static async Task<string> GetConnectionString(string executingDirectory)
+    {
+        const string defaultConnectionString = "Host=HOSTNAME;Port=PORT;Username=USERNAME;Password=PASSWORD;Database=DATABASE";
+        if (!File.Exists(Path.Join(executingDirectory, "_ConnectionString.txt")))
+        {
+            await File.WriteAllTextAsync(Path.Join(executingDirectory, "_ConnectionString.txt"), defaultConnectionString);
+            Console.WriteLine("Connection string file doesn't exist. _ConnectionString.txt has been created.\nPress any key to exit.");
+            Console.ReadKey();
+            Environment.Exit(1);
+        }
+
+        var connectionString = await File.ReadAllTextAsync(Path.Join(executingDirectory, "_ConnectionString.txt"));
+        if (connectionString != defaultConnectionString) return connectionString;
+
+        Console.WriteLine("Connection string file unmodified. Please update _ConnectionString.txt to reflect your target!");
+        Console.WriteLine("Press any key to exit.");
+        Console.ReadKey();
+        Environment.Exit(1);
+
+        return connectionString;
+    }
+
+    private static async Task MigrateToPostgreSql(string connectionString, DatabaseContext sourceContext)
+    {
+        var postgresTargetOptions = new DbContextOptionsBuilder<PostgresqlDatabaseContext>()
             .UseNpgsql(connectionString)
             .Options;
 
-        var sourceContext = new SqliteDatabaseContext(sourceOptions);
-        var targetContext = new PostgresqlDatabaseContext(targetOptions);
-        AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+        await Migration.MigrateDataAsync(sourceContext, () => new PostgresqlDatabaseContext(postgresTargetOptions));
+    }
 
-        await Migration.MigrateDataAsync(sourceContext, targetContext);
+    private static async Task MigrateToMySql(string connectionString, DatabaseContext sourceContext)
+    {
+        var mySqlTargetOptions = new DbContextOptionsBuilder<MySqlDatabaseContext>()
+            .UseMySql(connectionString, ServerVersion.AutoDetect(connectionString))
+            .Options;
+
+        await Migration.MigrateDataAsync(sourceContext, () => new MySqlDatabaseContext(mySqlTargetOptions));
     }
 }
